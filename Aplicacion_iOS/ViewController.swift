efbe8311aa594d6d38bd667ec80531ed37e3793f
Jorge Caro Poza <jorge.caro@utmac0011.local>
2019-05-13 17:16:26 +0200

    Initial Commit

---
+++ Aplicacion_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aplicacion_iOS
+//
+//  Created by Jorge Caro Poza on 13/5/19.
+//  Copyright © 2019 Jorge Caro Poza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
